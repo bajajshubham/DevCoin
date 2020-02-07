ed27122872b925ee8d1a2b1b7c788d3839c6d4fa
shubhambajaj <bajajshubham.bs@gmail.com>
2020-02-07 16:04:24 +0530

    update

---
+++ src/blocksChain/RunDemo.java
@@ -0,0 +1,15 @@
+package blocksChain;
+
+public class RunDemo {
+
+	public static void main(String[] args) {
+		Block genesisBlock = new Block("Genius Block","0");
+		System.out.println("Hash of Block1: "+genesisBlock.getHash());
+		Block firstBlock  = new Block("I am the first block",genesisBlock.getHash());
+		System.out.println("Hash of Block2: "+firstBlock.getHash());
+		Block secondBlock  = new Block("I am the second block",firstBlock.getHash());
+		System.out.println("Hash of Block3:"+secondBlock.getHash());
+
+	}
+
+}
