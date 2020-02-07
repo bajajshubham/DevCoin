d734f4e0189b68f9c4b6a0f6ba2d800e04e39f44
shubhambajaj <bajajshubham.bs@gmail.com>
2020-02-07 13:55:51 +0530

    block added

--- src/blocksdev/Block.java
+++ src/blocksdev/Block.java
@@ -1,5 +1,23 @@
 package blocksdev;
 
+import java.util.Date;
+
 public class Block {
 
+		//Number of milliseconds since 1/1/1970
+		private long timestamp;
+		private String data;
+		//current block hash
+		private String hash;
+		//previous block hash
+		private String prevBlockHash;
+		
+		//no role of default constructor here as this is block 
+		
+		public Block(String data, String prevBlockHash) {
+			this.data = data;
+			this.prevBlockHash=prevBlockHash;
+			this.timestamp = new Date().getTime();
+		}
+		
 }
