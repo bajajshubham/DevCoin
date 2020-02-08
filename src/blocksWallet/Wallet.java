28b2f3caf97d24f4fec1d2fa340f46561cefd6b6
shubhambajaj <bajajshubham.bs@gmail.com>
2020-02-08 16:51:06 +0530

    KeyPairGenr

--- src/blocksWallet/Wallet.java
+++ src/blocksWallet/Wallet.java
@@ -1,7 +1,13 @@
 package blocksWallet;
 
-import java.security.PublicKey;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
 
@@ -17,10 +23,46 @@ public class Wallet {
 	public PrivateKey getPrivateKey() {
 		return privateKey;
 	}
-	
+
+	//TODO WHY EACH OUTPUT IS PRINTING TWICE WITH DIFF_VALUES
 	//generates public and private key pair
 	//using EllipticCurveCryptography
 	public void generateKeyPair() {
+		
+		try {
+			
+			//ECDSA is Algorithm
+			//BC is provider
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA","BC");
+			//System.out.println("KeyPairGenerator: "+keyPairGenerator);
+			
+			//SHA1PRNG is Algorithm
+			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+			//System.out.println("SecureRandom: "+secureRandom);
+			
+			//TODO do not knew about it
+			ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("prime192v1");
+			//System.out.println("ECGenParameterSpec: "+ecGenParameterSpec);
+			
+			//Initializing key generator and generating a key pair
+			//algorithm parameter specification and secureRandom are parameters here
+			keyPairGenerator.initialize(ecGenParameterSpec, secureRandom);
+			//System.out.println("KeyPairGenerator: "+keyPairGenerator);
+			
+			KeyPair keyPair = keyPairGenerator.generateKeyPair();
+			//System.out.println("KeyPair: "+keyPair);
+			//getting and setting public && private keys from key-pair-generator
+			privateKey = keyPair.getPrivate();
+			publicKey = keyPair.getPublic();
+			
+			//System.out.println("PublicKey: "+publicKey);
+			//System.out.println("PrivateKey: "+privateKey);
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		}catch (Exception exception) { exception.printStackTrace(); }
 		
 	}
 	
