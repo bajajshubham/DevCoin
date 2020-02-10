88820403ca9d6c1969f32bfd31a3bb5f414f52c5
shubhambajaj <bajajshubham.bs@gmail.com>
2020-02-10 12:00:11 +0530

    Transactions IN's && OUT's

--- src/blocksWallet/TransactionsInputs.java
+++ src/blocksWallet/TransactionsInputs.java
@@ -1,5 +1,18 @@
 package blocksWallet;
 
+//used to reference the transaction outputs that have not been spent yet
 public class TransactionsInputs {
 
+	//Reference to transaction output
+	//It is used to relevant transaction output
+	//allowing miners to check ownership
+	public String transactionOutputID;
+	
+	//contains the unspent transaction output
+	public TransactionsOutputs UTXOS;
+	
+	public TransactionsInputs(String transactionOutputID) {
+		this.transactionOutputID=transactionOutputID;
+	}
+	
 }
--- src/blocksWallet/TransactionsOutputs.java
+++ src/blocksWallet/TransactionsOutputs.java
@@ -1,5 +1,33 @@
 package blocksWallet;
 
+import java.security.PublicKey;
+
+//These when referenced as inputs in new transactions
+//acts as proof value to spend
+//It also shows the final amount sent to each party from transaction
 public class TransactionsOutputs {
-		
+
+	public String id;
+	//New owner of the value
+	public PublicKey recipent;
+	//amount of coins owned
+	public float value;
+	//
+	//id of the transaction this output was created
+	public String parentTransactionID;
+	
+	public TransactionsOutputs(PublicKey recipent,float value,String parentTransactionID) {
+		this.recipent=recipent;
+		this.value=value;
+		this.parentTransactionID=parentTransactionID;
+		this.id = Utility.applySHA256(Utility.getStringFromKey(this.recipent)+
+				Float.toString(this.value)+
+				this.parentTransactionID);
+	}
+	
+	//check coins belong to whom
+	//
+	public boolean isMine(PublicKey publicKey) {
+		return (recipent==publicKey);
+	}
 }
