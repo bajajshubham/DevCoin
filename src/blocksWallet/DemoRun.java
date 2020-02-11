969f7f3f224a975b11212db332901878c304c997
shubhambajaj <bajajshubham.bs@gmail.com>
2020-02-11 16:44:17 +0530

    Final

--- src/blocksWallet/DemoRun.java
+++ src/blocksWallet/DemoRun.java
@@ -5,29 +5,33 @@ import java.security.Security;
 public class DemoRun {
 
 	public static void main(String[] args) {
-		
-		//BouncyCastle as Security Provider
+
+		// BouncyCastle as Security Provider
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-		
-		//manually process
+
+		// manually process
 		Wallet coinBase = new Wallet();
 		Wallet walletA = new Wallet();
 		Wallet walletB = new Wallet();
-		
-		//manually setup
+
+		// manually setup
 		Transaction genesisTransaction = new Transaction(coinBase.publicKey, walletA.publicKey, 100f, null);
 		Transaction genesisTransaction1 = new Transaction(coinBase.publicKey, walletB.publicKey, 101f, null);
 		genesisTransaction.generateSignature(coinBase.getPrivateKey());
 		genesisTransaction1.generateSignature(coinBase.getPrivateKey());
-		genesisTransaction.transactionID="0";
-		genesisTransaction1.transactionID="1";
-		genesisTransaction.transactionsOutputs.add(new TransactionsOutputs(walletA.publicKey, 100f, genesisTransaction.transactionID));
-		genesisTransaction1.transactionsOutputs.add(new TransactionsOutputs(walletB.publicKey, 101f, genesisTransaction1.transactionID));
-		BlkChain.UTXOs.put(genesisTransaction.transactionsOutputs.get(0).id, genesisTransaction.transactionsOutputs.get(0));
-		BlkChain.UTXOs.put(genesisTransaction1.transactionsOutputs.get(0).id, genesisTransaction1.transactionsOutputs.get(0));
-		
+		genesisTransaction.transactionID = "0";
+		genesisTransaction1.transactionID = "1";
+		genesisTransaction.transactionsOutputs
+				.add(new TransactionsOutputs(walletA.publicKey, 100f, genesisTransaction.transactionID));
+		genesisTransaction1.transactionsOutputs
+				.add(new TransactionsOutputs(walletB.publicKey, 101f, genesisTransaction1.transactionID));
+		BlkChain.UTXOs.put(genesisTransaction.transactionsOutputs.get(0).id,
+				genesisTransaction.transactionsOutputs.get(0));
+		BlkChain.UTXOs.put(genesisTransaction1.transactionsOutputs.get(0).id,
+				genesisTransaction1.transactionsOutputs.get(0));
+
 		System.out.println(genesisTransaction);
 		System.out.println(genesisTransaction1);
-		
+
 	}
 }
--- src/blocksWallet/Wallet.java
+++ src/blocksWallet/Wallet.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-
 //TODO
 //Keeping a record of transaction history
 
@@ -20,132 +19,112 @@ public class Wallet {
 
 	public PublicKey publicKey;
 	private PrivateKey privateKey;
-	
-	public HashMap<String,TransactionsOutputs> UTXOs = new HashMap<>();
-	
-	public Wallet() { generateKeyPair(); } 
-	
+
+	public HashMap<String, TransactionsOutputs> UTXOs = new HashMap<>();
+
+	public Wallet() {
+		generateKeyPair();
+	}
+
 	public PrivateKey getPrivateKey() {
 		return privateKey;
 	}
-	
-	
-	//TODO WHY EACH OUTPUT IS PRINTING TWICE WITH DIFF_VALUES
-	//generates public and private key pair
-	//using EllipticCurveCryptography
+
+	// TODO WHY EACH OUTPUT IS PRINTING TWICE WITH DIFF_VALUES
+	// generates public and private key pair
+	// using EllipticCurveCryptography
 	public void generateKeyPair() {
-		
+
 		try {
-			
-			//ECDSA is Algorithm
-			//BC is provider
-			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA","BC");
-			//System.out.println("KeyPairGenerator: "+keyPairGenerator);
-			
-			//SHA1PRNG is Algorithm
+
+			// ECDSA is Algorithm
+			// BC is provider
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
+			// System.out.println("KeyPairGenerator: "+keyPairGenerator);
+
+			// SHA1PRNG is Algorithm
 			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-			//System.out.println("SecureRandom: "+secureRandom);
-			
-			//TODO do not knew about it
+			// System.out.println("SecureRandom: "+secureRandom);
+
+			// TODO do not knew about it
 			ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("prime192v1");
-			//System.out.println("ECGenParameterSpec: "+ecGenParameterSpec);
-			
-			//Initializing key generator and generating a key pair
-			//algorithm parameter specification and secureRandom are parameters here
+			// System.out.println("ECGenParameterSpec: "+ecGenParameterSpec);
+
+			// Initializing key generator and generating a key pair
+			// algorithm parameter specification and secureRandom are parameters here
 			keyPairGenerator.initialize(ecGenParameterSpec, secureRandom);
-			//System.out.println("KeyPairGenerator: "+keyPairGenerator);
-			
+			// System.out.println("KeyPairGenerator: "+keyPairGenerator);
+
 			KeyPair keyPair = keyPairGenerator.generateKeyPair();
-			//System.out.println("KeyPair: "+keyPair);
-			//getting and setting public && private keys from key-pair-generator
+			// System.out.println("KeyPair: "+keyPair);
+			// getting and setting public && private keys from key-pair-generator
 			privateKey = keyPair.getPrivate();
 			publicKey = keyPair.getPublic();
-			
-			//System.out.println("PublicKey: "+publicKey);
-			//System.out.println("PrivateKey: "+privateKey);
-			
+
+			// System.out.println("PublicKey: "+publicKey);
+			// System.out.println("PrivateKey: "+privateKey);
+
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (NoSuchProviderException e) {
 			e.printStackTrace();
-		}catch (Exception exception) { exception.printStackTrace(); }
-		
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
 	}
-	
-	//returns the unspent balance
-	//Store the UTXO's owned by this wallet 
-	//in its UTXOs list
+
+	// returns the unspent balance
+	// Store the UTXO's owned by this wallet
+	// in its UTXOs list
 	public float getBalance() {
 		float total = 0;
-		
-		for(Map.Entry<String, TransactionsOutputs> item: BlkChain.UTXOs.entrySet()) {
-			System.out.println("Get Map.Entry<String, TransactionsOutputs> itemValue: "+item.getValue());
+
+		for (Map.Entry<String, TransactionsOutputs> item : BlkChain.UTXOs.entrySet()) {
+			// System.out.println("Get Map.Entry<String, TransactionsOutputs> itemValue:
+			// "+item.getValue());
 			TransactionsOutputs UTXO = item.getValue();
-			//If coin belongs to wallet 
-			//add it to list of
-			//unspent transactions
-			if(UTXO.isMine(publicKey)) {
-				//append
-				UTXOs.put(UTXO.id,UTXO);
-				System.out.println("GetBalance TransactionOutputs Value: "+UTXO.value);
-				total+=UTXO.value;
+			// If coin belongs to wallet
+			// add it to list of
+			// unspent transactions
+			if (UTXO.isMine(publicKey)) {
+				// append
+				UTXOs.put(UTXO.id, UTXO);
+				// System.out.println("GetBalance TransactionOutputs Value: "+UTXO.value);
+				total += UTXO.value;
 			}
 		}
-		
-		System.out.println("Get Balance Total: "+total);
+
+		// System.out.println("Get Balance Total: "+total);
 		return total;
 	}
-	
-	
-	//generates a new transactions
-	//from this.wallet or this wallet
-	//that is send funds
-	public Transaction sendFunds(PublicKey _recipient,float value) {
-		
-		//check first enough balance 
-		//to proceed transaction
-		if(getBalance() < value ) {
+
+	// generates a new transactions
+	// from this.wallet or this wallet
+	// that is send funds
+	public Transaction sendFunds(PublicKey _recipient, float value) {
+
+		// check first enough balance
+		// to proceed transaction
+		if (getBalance() < value) {
 			System.out.println("#TRANSACTION Not Enough Funds to send, Transaction Discarded ");
 			return null;
 		}
-		
+
 		ArrayList<TransactionsInputs> transactionsInputs = new ArrayList<>();
-		float total=0;
-		
-		for(Map.Entry<String,TransactionsOutputs> item: UTXOs.entrySet()) {
-			
+		float total = 0;
+
+		for (Map.Entry<String, TransactionsOutputs> item : UTXOs.entrySet()) {
+
 			TransactionsOutputs UTXO = item.getValue();
-			total  += UTXO.value;
+			total += UTXO.value;
 			transactionsInputs.add(new TransactionsInputs(UTXO.id));
-			
-			if(total > value) break;
+
+			if (total > value)
+				break;
 		}
-		
-		return (new Transaction(publicKey,_recipient,value,transactionsInputs));
+
+		return (new Transaction(publicKey, _recipient, value, transactionsInputs));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
